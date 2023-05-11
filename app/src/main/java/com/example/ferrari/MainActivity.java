c1185ebcb2222b427cedd190daea17e28f4d2421
Daniel Penelva <d4n.andrade@gmail.com>
2023-05-11 02:01:33 -0300

    Criando lista titulos, icones e descricao e Class ViewCarros

--- app/src/main/java/com/example/ferrari/MainActivity.java
+++ app/src/main/java/com/example/ferrari/MainActivity.java
@@ -5,29 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] listaTitulos = {"Ferrari 456", "Ferrari 308 GTB", "Ferrari F40", "Ferrari F50", "Ferrari F355",
+            "Ferrari 550", "Ferrari Enzo", "Ferrari F430", "Ferrari FF", "Ferrari LaFerrari"};
+
+    int[] listaIcones = {R.drawable.car1, R.drawable.car2, R.drawable.car3, R.drawable.car4, R.drawable.car5,
+            R.drawable.car6, R.drawable.car7, R.drawable.car8, R.drawable.car9, R.drawable.car10};
+
+    String[] listaDescricao = {"A 456 é um automóvel grand tourer da Ferrari. Equipado com motor V12, teve duas versões bases: a primeira, produzida até 1997, denominada apenas 456 e uma segunda versão, denominada 456M (a da foto), produzida de 1998 a 2003",
+            "Apresentado em 1975 no Salão do Automóvel de Paris, a Pininfarina usou para produzir o 308 GTB componentes mecânicos do 308 GT4 e teve sua inspiração estilística do 246 GT e 365 GT4 BB.",
+            "A Ferrari F40 é um automóvel superesportivo coupé de duas portas, de tração traseira e motor central-traseiro, projetado com conceitos aerodinâmicos utilizados em carros de competição. Porém, seu foco de vendas era estritamente comercial, não sendo planejado para corridas.",
+            "A Ferrari F50 é um supercarro de motor central-traseiro, duas portas, roadster, produzido pela marca italiana Ferrari que foi apresentado em 1995 para celebrar o quinquagésimo aniversário da companhia. O carro possui motor aspirado de 4.7L V12 com 60 válvulas (3 de admissão e 2 de exaustão por cilindro), que levava o carro à velocidade máxima de 328 km/h.",
+            "O Ferrari F355 é um carro desportivo construído pela Ferrari entre 1994 e 1999. É uma evolução do Ferrari 348, e foi substituído pelo Ferrari 360. Seguindo a linha de sucesso dos modelos de entrada da marca, o F355 é coupé V8 de motor central e dois lugares.",
+            "A 550 Maranello é um super esportivo da Ferrari equipado com motor V12 5.5 DOHC de 485cv, e atinge a velocidade máxima de 320 km/h. O número do modelo refere-se ao deslocamento total do motor (5.5 litros) e o nome do modelo de Maranello refere-se à cidade onde a sede da Ferrari estão localizados.",
+            "O carro foi desenvolvido em 2002 com tecnologias usadas na Fórmula 1.  Recebeu o nome do criador da companhia, Enzo Anselmo Ferrari. O motor 6.0 V12 com 48 válvulas foi o primeiro da nova geração da Ferrari. O seu desenho é baseado no V8 do Maserati Quattroporte.",
+            "O design do F430 foi desenvolvido em conjunto pela Ferrari e pelo estúdio Pininfarina. O estilo se inspira em antigos carros de corrida, com destaque para o modelo 1961, campeão do mundo de Fórmula 1 com Phil Hill, e também nos modernos - na traseira, ele é uma cópia reduzida do modelo Enzo.",
+            "O modelo foi a primeira Ferrari para quatro adultos. O motor, um V12 6.3 de 660 cv. O modelo causou polêmica por utilizar um sistema de tração nas quatro rodas, tecnologia condenada pelos puristas da marca.",
+            "É o primeiro modelo com tecnologia híbrida da marca. A associação de um motor V12 6.3 traseiro de 800 cv a um elétrico de 163 cv entrega a potência combinada de 963 cv. O câmbio é automatizado de sete marchas e a máxima de 350 km/h. Com 499 unidades, ela ainda está em produção, mas os exemplares já se esgotaram."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void irSegundaTela(View view){
-
-        //Recebe como parâmetro a tela em que está e a tela em que quer ir
-        Intent intent = new Intent(MainActivity.this, SegundaTela.class);
-
-        // Passando parâmetro entre as telas
-        intent.putExtra("modeloFerrari", "Ferrari 456");
-
-        startActivity(intent);
-    }
 }
+
+// Classe que declara os componentes que utilizaremos
+class ViewCarros{
+    ImageView imageIcone;
+    TextView titulo, descricao;
+}
+
+
+
+
+
+
 
 /* No Androidmanifest.xml vamos modificar o tema para NoActionBar, faremos a seguinte modificação:
 * android:theme="@style/Theme.AppCompat.Light.NoActionBar"
-*
 * */
